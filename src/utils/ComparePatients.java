7d33a2a75b9849aac77e4c671a2d5ddbb0e1c62d
AllanCapistrano <allan.capistrano3@gmail.com>
2021-09-03 21:01:13 -0300

    Add custom compare.

---
+++ src/utils/ComparePatients.java
@@ -0,0 +1,24 @@
+package utils;
+
+import java.util.Comparator;
+import models.PatientDevice;
+
+/**
+ * Compara dois pacientes em estado grave com base no nível de crítico dos
+ * mesmos
+ *
+ * @author Allan Capistrano
+ */
+public class ComparePatients implements Comparator<PatientDevice> {
+
+    @Override
+    public int compare(PatientDevice pd1, PatientDevice pd2) {
+        if (pd1.getPatientSeverityLevel() > pd2.getPatientSeverityLevel()) {
+            return -1;
+        } else if (pd1.getPatientSeverityLevel() > pd2.getPatientSeverityLevel()) {
+            return 1;
+        }
+
+        return 0;
+    }
+}
